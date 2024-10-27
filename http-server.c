72ea5d149aba698c0eb8174a404011bbc5ba08f8
Arindam-langer <arindamhateswork@gmail.com>
2024-10-27 22:23:51 +0530

    created a loop for accepting clients and added an if condition to check for invalid socet

--- http-server.c
+++ http-server.c
@@ -2,28 +2,32 @@
 #include <stdlib.h>
 #include <winsock2.h>
 
-int main() {
-    WSADATA wsa; // Corrected from WSANO_DATA to WSADATA
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+int main() 
+{
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) 
+    {
         printf("WSAStartup failed. Error Code: %d\n", WSAGetLastError());
         exit(1);
     }
-
+//end of function
     // Creating a socket
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == INVALID_SOCKET) {
+    if (server_socket == INVALID_SOCKET) 
+    {
         printf("Socket creation failed. Error Code: %d\n", WSAGetLastError());
         WSACleanup();
         exit(1);
     }
-
+///end of function
     // Binding socket to a port number
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8080); // Port number
     server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to all interfaces
 
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) 
+    {
         printf("Bind failed. Error Code: %d\n", WSAGetLastError());
         closesocket(server_socket);
         WSACleanup();
@@ -31,7 +35,8 @@ int main() {
     }
 
     // Listening for incoming connections with a queue size of 5
-    if (listen(server_socket, 5) == SOCKET_ERROR) {
+    if (listen(server_socket, 5) == SOCKET_ERROR) 
+    {
         printf("Listen failed. Error Code: %d\n", WSAGetLastError());
         closesocket(server_socket);
         WSACleanup();
@@ -41,22 +46,29 @@ int main() {
     printf("Waiting for client connections...\n");
 
     // Accepting clients
-    struct sockaddr_in client_addr;
-    int addr_len = sizeof(client_addr);
-    int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_len);
-    if (client_socket == INVALID_SOCKET) {
-        printf("Accept failed. Error Code: %d\n", WSAGetLastError());
-        closesocket(server_socket);
-        WSACleanup();
-        exit(1);
+    while(1)
+    {
+        struct sockaddr_in client_addr;
+        int addr_len = sizeof(client_addr);
+        int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_len);
+        if (client_socket == INVALID_SOCKET) {
+            printf("Accept failed. Error Code: %d\n", WSAGetLastError());
+            continue;  // Continue to accept new connections
+        }
+
+        // Sending HTTP response
+        char response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Hello, World!</h1>";
+        int send_result = send(client_socket, response, sizeof(response) - 1, 0); // Use sizeof(response) - 1 to avoid sending null terminator
+        if (send_result == SOCKET_ERROR)
+        {
+            printf("Send failed. Error Code: %d\n", WSAGetLastError());
+        }
+
+        // Close client socket after sending the response
+        closesocket(client_socket);
     }
-
-    // Sending HTTP response
-    char response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Hello, World!</h1>";
-    send(client_socket, response, sizeof(response) - 1, 0); // Use sizeof(response) - 1 to avoid sending null terminator
-
     // Closing sockets
-    closesocket(client_socket);
+
     closesocket(server_socket);
     WSACleanup();
 
